91e01d4b2fbff2bbedc1deab1c9b35289f872e91
Jinesh Gandhi <jineshgandhi79@gmail.com>
2025-04-19 22:16:42 +0530

    bookmarks route

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,6 +5,7 @@ import { User } from 'lucide-react';
 function NavBar() {
   const location = useLocation();
   const isJobsRoute = location.pathname === '/jobs';
+  const isBookMarkRoute = location.pathname === '/bookmarks';
   const isProfileRoute = location.pathname === '/profile';
 
   return (
@@ -15,13 +16,20 @@ function NavBar() {
         </Link>
       </div>
       
-      <div className="flex items-center justify-center flex-2">
+      <div className="flex items-center justify-center gap-2 flex-1">
         <Link to={"/jobs"} 
-          className={`mx-4 text-white font-medium px-3 py-2 rounded-md transition-all duration-300 hover:bg-white/10 relative
+          className={`text-white font-medium px-3 py-2 rounded-md transition-all duration-300 hover:bg-white/10 relative
           after:content-[''] after:absolute after:h-0.5 after:bottom-0 after:left-1/2 after:-translate-x-1/2 
           after:bg-white after:transition-all after:duration-300 
           ${isJobsRoute ? 'bg-white/10 after:w-4/5' : 'after:w-0 hover:after:w-4/5'}`}>
           Jobs
+        </Link>
+        <Link to={"/bookmarks"} 
+          className={`text-white font-medium px-3 py-2 rounded-md transition-all duration-300 hover:bg-white/10 relative
+          after:content-[''] after:absolute after:h-0.5 after:bottom-0 after:left-1/2 after:-translate-x-1/2 
+          after:bg-white after:transition-all after:duration-300 
+          ${isBookMarkRoute ? 'bg-white/10 after:w-4/5' : 'after:w-0 hover:after:w-4/5'}`}>
+          Bookmarks
         </Link>
       </div>
       
